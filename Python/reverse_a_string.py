eae2794e489625817d4dda5e068d4221b32f5001
muhsinaids <muhsinbinmubarak11@gmail.com>
2026-09-11 20:40:22 +0530

    feat: updation

--- Python/reverse_a_string.py
+++ Python/reverse_a_string.py
@@ -1,5 +1,6 @@
 str=input("Enter a string to reverse: ")
+print(f"The string is :{str}")
 str2=""
 for i in str:
     str2=i+str2
-print("Reversed string is:", str2)
+print(f"Reversed string is:{str2}")
