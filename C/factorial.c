b7607646eaa73b325da66d726d9e0e14641c4380
muhsinaids <muhsinbinmubarak11@gmail.com>
2026-09-14 18:09:21 +0530

    Feat: comparison factorial with py and java

---
+++ C/factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int number;
+    int fact;
+    
+    printf("Enter a number:");
+    scanf("%d", &number);
+     
+    for(int i=1; i<=number; i++){
+        fact = fact * i;
+    }
+    printf("Factorial of %d is %d", number, fact);
+    return 0;
+}
