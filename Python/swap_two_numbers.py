202238c7c24ed3980a9ba5bbad4d8626e4b65d38
muhsinaids <muhsinbinmubarak11@gmail.com>
2026-09-20 22:02:53 +0530

    Feat: code comparison within java and c

---
+++ Python/swap_two_numbers.py
@@ -0,0 +1,13 @@
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+
+print("--------------------------------")
+print(f"The first number is: {num1}")
+print(f"The second number is: {num2}")
+print("--------------------------------")
+num1 = num1^num2
+num2 = num1^num2
+num1 = num1^num2
+
+print(f"The first number after swapping: {num1}")
+print(f"The second number after swapping: {num2}")
